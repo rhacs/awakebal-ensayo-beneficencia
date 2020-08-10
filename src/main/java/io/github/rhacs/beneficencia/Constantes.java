3f39f838d31f998a95a65bafc53a255f1aad811e
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-10 19:28:50 -0400

    added class Constantes

---
+++ src/main/java/io/github/rhacs/beneficencia/Constantes.java
@@ -0,0 +1,17 @@
+package io.github.rhacs.beneficencia;
+
+public class Constantes {
+
+    public static final String TABLA_BENEFICIARIOS = "beneficiarios";
+    public static final String TABLA_AYUDAS = "ayudas";
+    public static final String TABLA_CIUDADES = "ciudades";
+
+    public static final String SECUENCIA_BENEFICIARIOS = "beneficiarios_seq";
+    public static final String SECUENCIA_AYUDAS = "ayudas_seq";
+    public static final String SECUENCIA_CIUDADES = "ciudades";
+
+    public Constantes() {
+        // Constructor privado para esconder el constructor público implícito
+    }
+    
+}
