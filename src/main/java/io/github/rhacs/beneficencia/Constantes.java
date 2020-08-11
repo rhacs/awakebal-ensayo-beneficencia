bb41d5e49a4b21edeabe0293cbf69fcb9dcb17b6
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-10 20:30:38 -0400

    fixed constructor access modifier

--- src/main/java/io/github/rhacs/beneficencia/Constantes.java
+++ src/main/java/io/github/rhacs/beneficencia/Constantes.java
@@ -2,16 +2,28 @@ package io.github.rhacs.beneficencia;
 
 public class Constantes {
 
+    // Tablas
+    // -----------------------------------------------------------------------------------------
+
     public static final String TABLA_BENEFICIARIOS = "beneficiarios";
     public static final String TABLA_AYUDAS = "ayudas";
     public static final String TABLA_CIUDADES = "ciudades";
+
+    //
+    // -----------------------------------------------------------------------------------------
 
     public static final String SECUENCIA_BENEFICIARIOS = "beneficiarios_seq";
     public static final String SECUENCIA_AYUDAS = "ayudas_seq";
     public static final String SECUENCIA_CIUDADES = "ciudades_seq";
 
-    public Constantes() {
+    // Constructores
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Crea una nueva instancia del objeto {@link Constantes}
+     */
+    private Constantes() {
         // Constructor privado para esconder el constructor público implícito
     }
-    
+
 }
